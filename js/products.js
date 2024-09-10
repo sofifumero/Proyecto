3c88c7da4d60ed0af149db0f4459983886b2c579
agustinauran <agustina.uran@hotmail.com>
2024-09-09 21:50:45 -0300

    Se agrega busquedor de productos

---
+++ js/products.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("searchProductInput");
+  const productContainer = document.getElementById('product-container');
+  searchInput.addEventListener("input", (event) => {
+    const searchText = searchInput.value.toLowerCase();
+    const productCards = productContainer.getElementsByClassName('card');
+
+    for (let productCard of productCards) {
+      let include = true;
+
+      const nameParagraph = productCard.getElementsByClassName('card-text')[0];
+      const descriptionParagraph = productCard.getElementsByClassName('info-card')[0];
+
+      if (nameParagraph && descriptionParagraph) {
+        include =
+          nameParagraph.textContent?.toLowerCase()?.includes(searchText) ||
+          descriptionParagraph.textContent?.toLowerCase()?.includes(searchText)
+      }
+
+      productCard.style.display = include ? "block" : "none";
+    }
+  });
+});
